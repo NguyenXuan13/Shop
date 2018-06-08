ec220356c322c3810eb80d0d610db61b60074d40
truonghoang81 <truongviethoang30111996@gmail.com>
2018-06-08 20:27:29 +0700

    clean code

--- ShopOnline/ShopOnline/ShopOnline.Web/Models/TagViewModel.cs
+++ ShopOnline/ShopOnline/ShopOnline.Web/Models/TagViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ShopOnline.Web.Models
+﻿namespace ShopOnline.Web.Models
 {
     public class TagViewModel
     {
