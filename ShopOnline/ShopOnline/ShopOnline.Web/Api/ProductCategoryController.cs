a08c936550776bacd28da8897a2f7e2411cb8367
DESKTOP-DRVPD1L\XuanNguyen <eragonzappy@gmail.com>
2018-06-13 07:17:02 +0700

    Xóa nhiều bản ghi

--- ShopOnline/ShopOnline/ShopOnline.Web/Api/ProductCategoryController.cs
+++ ShopOnline/ShopOnline/ShopOnline.Web/Api/ProductCategoryController.cs
@@ -10,6 +10,7 @@ using ShopOnline.Service;
 using ShopOnline.Web.Infrastructure.Core;
 using ShopOnline.Web.Models;
 using ShopOnline.Web.Infrastructure.Extensions;
+using System.Web.Script.Serialization;
 
 namespace ShopOnline.Web.Api
 {
@@ -161,6 +162,35 @@ namespace ShopOnline.Web.Api
 
                     var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(oldProductCategory);
                     response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("deletemulti")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage DeleteMulti(HttpRequestMessage request, string checkedProductCategories)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listProductCategory = new JavaScriptSerializer().Deserialize<List<int>>(checkedProductCategories);
+                    foreach (var item in listProductCategory)
+                    {
+                        _productCategoryService.Delete(item);
+                    }
+
+                    _productCategoryService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK, listProductCategory.Count);
                 }
 
                 return response;
