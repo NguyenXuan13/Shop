aef5f7144c599c86310f03a95e61a8e8a3ea2354
DESKTOP-DRVPD1L\XuanNguyen <eragonzappy@gmail.com>
2018-06-17 00:29:36 +0700

    no message

--- ShopOnline/ShopOnline/ShopOnline.Web/Models/ProductViewModel.cs
+++ ShopOnline/ShopOnline/ShopOnline.Web/Models/ProductViewModel.cs
@@ -46,6 +46,7 @@ namespace ShopOnline.Web.Models
 
         public bool Status { set; get; }
         public string Tags { set; get; }
+        public int? Quantity { set; get; }
 
         public virtual ProductCategoryViewModel ProductCategory { set; get; }
     }
